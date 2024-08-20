c75354d0ca627ddfbe47ae413ae6e6850f1f1442
murilosopi <dev.murilosopi@gmail.com>
2024-08-19 23:22:03 -0300

    initialize AboutView route

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  { path: "/", redirect: "/quem-somos" },
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
+    path: "/quem-somos",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
