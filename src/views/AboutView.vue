fc57c75cd0d3629e9dc51f5baaa2be6fbc9744b7
murilosopi <dev.murilosopi@gmail.com>
2024-08-19 23:22:31 -0300

    add MainTitle into AboutView component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <article class="home">
+    <MainTitle :tag="'h1'" class="text-center">
+      <i class="fas fa-users"></i>
+      Quem somos
+    </MainTitle>
+  </article>
 </template>
+
+<script>
+import MainTitle from "@/components/MainTitle.vue";
+
+export default {
+  name: "AboutView",
+  components: { MainTitle },
+};
+</script>
