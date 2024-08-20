7beba74049a7828c72dc052ed53be65baa92a662
murilosopi <dev.murilosopi@gmail.com>
2024-08-19 23:21:38 -0300

    modify NavigationHeader styles

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,13 +1,15 @@
 <template>
-  <nav class="flex justify-between p-8">
+  <nav
+    class="flex justify-between p-8 font-display text-xl items-center bg-gradient-to-b from-[#ffffff0d] via-[#9999990d]"
+  >
     <figure>
       <img src="@/assets/img/dark-trimboli-logo.svg" alt="Trimboli Logo" />
     </figure>
-    <ul class="flex gap-10">
-      <li>
+    <ul class="flex gap-10 items-end">
+      <li class="item">
         <router-link to="/quem-somos">Quem Somos</router-link>
       </li>
-      <li>
+      <li class="item">
         Produtos e Serviços
         <ul>
           <li>
@@ -20,10 +22,10 @@
           </li>
         </ul>
       </li>
-      <li>
+      <li class="item">
         <router-link to="/contato">Contato</router-link>
       </li>
-      <li>
+      <li class="item">
         <router-link to="/faq">Perguntas Frequentes</router-link>
       </li>
     </ul>
@@ -37,7 +39,11 @@ export default {
 </script>
 
 <style scoped>
-nav a.router-link-exact-active {
-  @apply text-primary-light;
+.item {
+  @apply border-b border-light hover:border-primary-muted hover:text-primary-muted pb-2 transition-all duration-150;
+}
+
+.item:has(.router-link-exact-active) {
+  @apply text-primary-light border-primary-light;
 }
 </style>
