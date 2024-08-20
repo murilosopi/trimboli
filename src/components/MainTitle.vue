d598f6dda51bb7ca29b1585d21f20f9d72222199
murilosopi <dev.murilosopi@gmail.com>
2024-08-19 23:21:18 -0300

    create MainTitle component

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="tag" class="text-primary font-bold text-5xl font-display">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "MainTitle",
+  props: {
+    tag: {
+      type: String,
+      default: "h1",
+    },
+  },
+};
+</script>
