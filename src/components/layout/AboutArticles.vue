b8bc0954319bcbe1a6beb6043697c4b70bb70002
murilosopi <dev.murilosopi@gmail.com>
2024-08-20 22:29:12 -0300

    create AboutArticles layout component

---
+++ src/components/layout/AboutArticles.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <ArticleContent title="Nossa essência" alignment="left" class="mt-12">
+      <p slot="content">
+        A <strong>Trimboli</strong> é uma empresa especializada em soluções de
+        securitização, locação e administração de imóveis, comprometida em
+        oferecer serviços de alta qualidade e confiança. Com uma trajetória
+        marcada pela dedicação e seriedade, buscamos sempre entregar resultados
+        que excedem as expectativas dos nossos clientes. Nossa atuação é pautada
+        pela transparência e pela eficiência, proporcionando segurança e
+        tranquilidade em cada etapa dos processos que conduzimos.
+      </p>
+    </ArticleContent>
+
+    <ArticleContent
+      title="Inovação e qualificação"
+      alignment="right"
+      class="mt-12"
+    >
+      <p slot="content">
+        Com uma equipe de profissionais altamente qualificados e uma
+        infraestrutura robusta, a Trimboli se destaca no mercado por sua
+        capacidade de adaptação e inovação. Trabalhamos continuamente para
+        aprimorar nossas práticas, incorporando as mais recentes tecnologias e
+        metodologias para garantir que nossos clientes recebam o melhor suporte
+        possível. Nosso foco é criar valor duradouro, seja na gestão de ativos
+        imobiliários ou na securitização de recebíveis, sempre com um olhar
+        atento às necessidades e objetivos específicos de cada cliente.
+      </p>
+    </ArticleContent>
+
+    <ArticleContent
+      title="Compromisso com a excelência"
+      alignment="left"
+      class="mt-12"
+    >
+      <p slot="content">
+        Acreditamos que nosso sucesso está diretamente ligado ao sucesso de
+        nossos clientes. Por isso, nos dedicamos a entender profundamente suas
+        necessidades, oferecendo soluções personalizadas que atendem às demandas
+        mais complexas com agilidade e precisão. Na Trimboli, nosso compromisso
+        é com a excelência, e estamos prontos para construir, junto com nossos
+        clientes, um futuro sólido e promissor.
+      </p>
+    </ArticleContent>
+  </div>
+</template>
+
+<script>
+import ArticleContent from "../common/ArticleContent.vue";
+
+export default {
+  name: "AboutArticles",
+  components: { ArticleContent },
+};
+</script>
