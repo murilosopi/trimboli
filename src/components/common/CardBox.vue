011237a8599ed04c10a515945ce621c02bafbb2d
murilosopi <dev.murilosopi@gmail.com>
2024-08-20 23:42:54 -0300

    create CardBox component

---
+++ src/components/common/CardBox.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="bg-gray">
+    <div>
+      <img v-if="img" :src="img" alt="" />
+    </div>
+    <div class="p-7">
+      <MainTitle
+        tag="h3"
+        class="text-3xl border-b-4 border-primary mx-auto w-fit mb-4"
+        >{{ title }}</MainTitle
+      >
+      <slot name="content"></slot>
+    </div>
+  </article>
+</template>
+
+<script>
+import MainTitle from "./MainTitle.vue";
+
+export default {
+  name: "CardBox",
+  props: {
+    img: String,
+    title: String,
+  },
+  components: { MainTitle },
+};
+</script>
