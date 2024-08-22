a27116f711ff563d0fdbaeb5362cc18bc2612087
murilosopi <dev.murilosopi@gmail.com>
2024-08-21 23:34:09 -0300

    create NavlistDefault as a partil of NavgationHeader

--- src/components/layout/NavigationHeader.vue
+++ src/components/layout/NavigationHeader.vue
@@ -1,74 +1,20 @@
 <template>
   <nav
-    class="flex justify-between p-8 font-display text-xl items-center bg-gradient-to-b from-[#ffffff0d] via-[#9999990d]"
+    class="flex justify-between p-8 font-display xl:text-xl lg:text-lg items-center bg-gradient-to-b from-[#ffffff0d] via-[#9999990d]"
   >
-    <figure>
+    <figure class="lg:w-auto w-20">
       <img src="@/assets/img/dark-trimboli-logo.svg" alt="Trimboli Logo" />
     </figure>
-    <ul class="flex gap-10 items-end">
-      <li class="item">
-        <router-link to="/quem-somos">Quem Somos</router-link>
-      </li>
-      <li
-        class="item relative cursor-pointer"
-        @click="openDropdownMenu"
-        @mouseenter="openDropdownMenu"
-        @mouseleave="closeDropdownMenu"
-      >
-        Produtos e Serviços <i class="fa-solid fa-chevron-down"></i>
-        <DropdownMenu
-          :items="items"
-          ref="dropdownMenu"
-          @mouseenter.native="openDropdownMenu"
-        />
-      </li>
-      <li class="item">
-        <router-link to="/contato">Contato</router-link>
-      </li>
-      <li class="item">
-        <router-link to="/faq">Perguntas Frequentes</router-link>
-      </li>
-    </ul>
+
+    <NavlistDefault class="hidden md:flex" />
   </nav>
 </template>
 
 <script>
-import DropdownMenu from "../common/DropdownMenu.vue";
+import NavlistDefault from "./NavlistDefault.vue";
 
 export default {
   name: "NavigationHeader",
-  components: { DropdownMenu },
-  data() {
-    return {
-      items: [
-        {
-          content: "Securitização",
-          to: "/securitizacao",
-        },
-        {
-          content: "Gestão Imobiliária",
-          to: "/gestao-imobiliaria",
-        },
-      ],
-    };
-  },
-  methods: {
-    openDropdownMenu() {
-      this.$refs.dropdownMenu.open();
-    },
-    closeDropdownMenu() {
-      this.$refs.dropdownMenu.close(1);
-    },
-  },
+  components: { NavlistDefault },
 };
 </script>
-
-<style scoped>
-.item {
-  @apply border-b border-light hover:border-primary-muted hover:text-primary-muted pb-2 transition-all duration-150;
-}
-
-.item:has(.router-link-exact-active) {
-  @apply text-primary-light border-primary-light;
-}
-</style>
---
+++ src/components/layout/NavlistDefault.vue
@@ -0,0 +1,72 @@
+<template>
+  <ul class="flex gap-5">
+    <li class="item">
+      <router-link to="/quem-somos">Quem Somos</router-link>
+    </li>
+    <li
+      class="item relative cursor-pointer"
+      @click="openDropdownMenu"
+      @mouseenter="openDropdownMenu"
+      @mouseleave="closeDropdownMenu"
+    >
+      Produtos e Serviços <i class="fa-solid fa-chevron-down"></i>
+      <DropdownMenu ref="dropdownMenu" @mouseenter.native="openDropdownMenu">
+        <DropdownOption
+          v-for="{ to, content } in servicesAndProdutcts"
+          :key="to"
+        >
+          <router-link :to="to"> {{ content }}</router-link>
+        </DropdownOption>
+      </DropdownMenu>
+    </li>
+    <li class="item">
+      <router-link to="/contato">Contato</router-link>
+    </li>
+    <li class="item">
+      <router-link to="/faq">Perguntas Frequentes</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import DropdownMenu from "../common/DropdownMenu.vue";
+import DropdownOption from "../common/DropdownOption.vue";
+
+export default {
+  name: "NavlistDefault",
+  components: { DropdownMenu, DropdownOption },
+  data() {
+    return {
+      servicesAndProdutcts: [
+        {
+          content: "Securitização",
+          to: "/securitizacao",
+        },
+        {
+          content: "Gestão Imobiliária",
+          to: "/gestao-imobiliaria",
+        },
+      ],
+      closed: true,
+    };
+  },
+  methods: {
+    openDropdownMenu() {
+      this.$refs.dropdownMenu.open();
+    },
+    closeDropdownMenu() {
+      this.$refs.dropdownMenu.close(0.5);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  @apply border-b border-light hover:border-primary-muted hover:text-primary-muted pb-2 transition-all duration-150;
+}
+
+.item:has(.router-link-exact-active) {
+  @apply text-primary-light border-primary-light;
+}
+</style>
