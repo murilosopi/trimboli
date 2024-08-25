f31e1b56fc8c51c803fac6d49af3802fffc209ad
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 18:51:10 -0300

    create MainButton component

---
+++ src/components/common/MainButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="bg-primary text-dark font-bold px-8 py-1 text-xl rounded">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MainButton",
+};
+</script>
