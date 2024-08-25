4186efb526cd4706e516d5022bd22cbf65c56338
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 15:44:36 -0300

    create DropdownOptionGroup

---
+++ src/components/common/DropdownOptionGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="px-3 py-2 text-dark" @click="collapse = !collapse">
+    <slot name="header"></slot>
+    <transition name="fade">
+      <div v-show="collapse">
+        <slot name="content"></slot>
+      </div>
+    </transition>
+  </li>
+</template>
+
+<script>
+import "@/styles/transitions/fade.css";
+
+export default {
+  name: "DropdownOptionGroup",
+  data() {
+    return {
+      collapse: false,
+    };
+  },
+};
+</script>
