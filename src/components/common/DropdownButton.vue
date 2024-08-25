79c5eb69f9e506d8d725762763b87102252ab5f0
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 18:50:56 -0300

    create DropdownButton component

---
+++ src/components/common/DropdownButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative">
+    <MainButton class="rounded-none rounded-l mr-[1px]">
+      <slot name="button"></slot>
+    </MainButton>
+    <MainButton
+      class="rounded-none rounded-r !px-3"
+      @click.native="toggleDropdown"
+    >
+      <i class="fa-solid fa-chevron-down text-xs"></i>
+    </MainButton>
+    <DropdownMenu ref="dropdown" class="mt-1">
+      <slot name="menu"></slot>
+    </DropdownMenu>
+  </div>
+</template>
+
+<script>
+import DropdownMenu from "./DropdownMenu.vue";
+import MainButton from "./MainButton.vue";
+
+export default {
+  name: "DropdownButton",
+  components: { MainButton, DropdownMenu },
+  methods: {
+    toggleDropdown() {
+      this.$refs.dropdown.toggle();
+    },
+  },
+};
+</script>
