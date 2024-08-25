cbed7d23200b6f8422b7226f2559888dc082ca9c
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 18:50:36 -0300

    create BannerContainer component

---
+++ src/components/common/BannerContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="py-20 min-h-[60vh] flex justify-center items-center bg-no-repeat bg-cover bg-bottom"
+    :style="{
+      backgroundImage: `url(${image})`,
+    }"
+  >
+    <ContainerView class="">
+      <slot></slot>
+    </ContainerView>
+  </div>
+</template>
+
+<script>
+import ContainerView from "../layout/ContainerView.vue";
+
+export default {
+  name: "BannerContainer",
+  props: ["image"],
+  components: {
+    ContainerView,
+  },
+};
+</script>
