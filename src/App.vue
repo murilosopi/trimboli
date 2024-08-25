291e550b61c082d4b948fe89261886b3b7036bdb
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 18:54:56 -0300

    remove general layout styles from App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app" class="text-light font-body bg-dark min-h-dvh">
-    <NavigationHeader class="mx-auto mb-5" />
-    <div class="container mx-auto px-4 max-w-5xl">
-      <router-view />
-    </div>
+    <NavigationHeader class="mx-auto" />
+    <router-view />
   </div>
 </template>
 <script>
