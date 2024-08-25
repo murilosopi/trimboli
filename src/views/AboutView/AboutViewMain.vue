7be1f7a307ad0ccae0ef22e37848b11e4240c93c
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 18:56:27 -0300

    add ContainerView to the AboutView

--- src/views/AboutView/AboutViewMain.vue
+++ src/views/AboutView/AboutViewMain.vue
@@ -1,25 +1,28 @@
 <template>
   <section class="home">
-    <MainTitle :tag="'h1'" class="text-center">
-      <i class="fas fa-users"></i>
-      Quem somos
-    </MainTitle>
+    <ContainerView>
+      <MainTitle :tag="'h1'" class="text-center">
+        <i class="fas fa-users"></i>
+        Quem somos
+      </MainTitle>
 
-    <AboutArticles />
+      <AboutArticles />
 
-    <hr class="w-2/3 mx-auto my-12 border-gray" />
+      <hr class="w-2/3 mx-auto my-12 border-gray" />
 
-    <AboutCards />
+      <AboutCards />
+    </ContainerView>
   </section>
 </template>
 
 <script>
+import ContainerView from "@/components/layout/ContainerView.vue";
 import MainTitle from "@/components/common/MainTitle.vue";
 import AboutArticles from "./fragments/AboutArticles.vue";
 import AboutCards from "./fragments/AboutCards.vue";
 
 export default {
   name: "AboutViewMain",
-  components: { MainTitle, AboutArticles, AboutCards },
+  components: { ContainerView, MainTitle, AboutArticles, AboutCards },
 };
 </script>
