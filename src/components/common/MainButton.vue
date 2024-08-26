40fcbccd867fd888d3bcdac6ddba2bd5ef812354
murilosopi <dev.murilosopi@gmail.com>
2024-08-26 03:25:49 -0300

    add varition to the MainButton component

--- src/components/common/MainButton.vue
+++ src/components/common/MainButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="bg-primary text-dark font-bold px-8 py-1 text-xl rounded">
+  <button
+    type="button"
+    class="font-bold px-8 py-1 text-base rounded hover:brightness-90 active:brightness-75 transition-all"
+    :class="variantStyles[variant]"
+  >
     <slot></slot>
   </button>
 </template>
@@ -7,5 +11,22 @@
 <script>
 export default {
   name: "MainButton",
+  props: {
+    variant: {
+      type: String,
+      default: "primary",
+      validator(value) {
+        return ["primary", "light"].includes(value);
+      },
+    },
+  },
+  data() {
+    return {
+      variantStyles: {
+        primary: "bg-primary text-dark focus:ring focus:ring-primary-muted",
+        light: "bg-light text-dark focus:ring focus:ring-gray",
+      },
+    };
+  },
 };
 </script>
