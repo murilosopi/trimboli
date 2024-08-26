c2f1c32c09b652012730d0de9c21f3ab4bee2c06
murilosopi <dev.murilosopi@gmail.com>
2024-08-25 23:23:55 -0300

    create AnchorLink component

---
+++ src/components/common/AnchorLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-link v-if="router" :to="href" class="link">
+    <slot></slot>
+  </router-link>
+  <a v-else :href="href" class="link">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "AnchorLink",
+  props: {
+    href: String,
+    router: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.link {
+  @apply text-primary-light font-bold hover:!underline;
+}
+</style>
