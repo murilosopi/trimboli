259b943ebc1b022631777bb0b9a316815a2f6765
murilosopi <dev.murilosopi@gmail.com>
2024-08-26 03:26:03 -0300

    change ContactViewMain content

--- src/views/ContactView/ContactViewMain.vue
+++ src/views/ContactView/ContactViewMain.vue
@@ -6,15 +6,11 @@
         Contato
       </MainTitle>
       <p>
-        Antes de entrar em contato, recomendamos que você visite nossa seção de
-        <AnchorLink href="/faq" :router="true"
-          >Perguntas Frequentes (FAQ)</AnchorLink
-        >, onde você poderá encontrar respostas para as dúvidas mais comuns.
-      </p>
-      <p>
-        Se precisar de assistência adicional, por favor, utilize o formulário
-        abaixo para nos enviar sua mensagem. Nossa equipe está pronta para
-        ajudá-lo e responderá o mais breve possível.
+        Estamos prontos para ouvir você! Utilize o formulário abaixo para entrar
+        em contato com a <strong>Trimboli</strong>. Preencha seus dados e
+        selecione o assunto de sua mensagem. Se preferir, escolha "Outro" e
+        personalize o tema de sua consulta. Nossa equipe responderá o mais
+        rápido possível.
       </p>
       <p>
         Caso prefira, você também pode nos contatar diretamente pelo e-mail
@@ -24,6 +20,7 @@
         ou pelo telefone
         <AnchorLink href="tel:(11) 5128-9560">(11) 5128-9560</AnchorLink>.
       </p>
+      <ContactForm class="mt-8" />
     </section>
   </ContainerView>
 </template>
@@ -32,9 +29,10 @@
 import ContainerView from "@/components/layout/ContainerView.vue";
 import MainTitle from "@/components/common/MainTitle.vue";
 import AnchorLink from "@/components/common/AnchorLink.vue";
+import ContactForm from "./fragments/ContactForm.vue";
 
 export default {
   name: "ContactViewMain",
-  components: { ContainerView, MainTitle, AnchorLink },
+  components: { ContainerView, MainTitle, AnchorLink, ContactForm },
 };
 </script>
